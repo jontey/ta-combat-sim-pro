22067bca10741043df1236db1d6673d41b77079b
PythEch <pythech.tr@gmail.com>
2012-06-25 02:27:59 +0300

    Unreverted some changes after NgXAlex's comment
    NgXAlex reported that one of the reverted fix was actually working

--- ta_sim.user.js
+++ ta_sim.user.js
@@ -225,8 +225,8 @@
                   info.h = unitData[i].get_Health();
                   info.i = unitData[i].get_MdbUnitId();
                   info.l = unitData[i].get_CurrentLevel();
-                  info.x = unitData[i].get_CoordX();
-				  info.y = unitData[i].get_CoordY();
+                  info.x = offense_units[i].get_CoordX();
+                  info.y = offense_units[i].get_CoordY();
                   data.push(info);
                 }
 
@@ -239,8 +239,8 @@
                   info.h = unitData[i].get_Health();
                   info.i = unitData[i].get_MdbUnitId();
                   info.l = unitData[i].get_CurrentLevel();
-                  info.x = unitData[i].get_CoordX();
-				  info.y = unitData[i].get_CoordY();
+                  info.x = offense_units[i].get_CoordX();
+                  info.y = offense_units[i].get_CoordY();
                   data.push(info);
                 }
                 combatData.UN = data; // Defenders
@@ -286,8 +286,8 @@
                   info.h = unitData[i].get_Health();
                   info.i = unitData[i].get_MdbUnitId();
                   info.l = unitData[i].get_CurrentLevel();
-                  info.x = unitData[i].get_CoordX();
-				  info.y = unitData[i].get_CoordY();
+                  info.x = offense_units[i].get_CoordX();
+                  info.y = offense_units[i].get_CoordY(); 
                   data.push(info);
                 }
                 
